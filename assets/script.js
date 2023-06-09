4993040c4eeb6840b47a820e262a9b4d2e8bd7d1
clayg <claytondguerrer@gmail.com>
2023-06-09 16:42:29 -0500

    added topics array and selectTopic func

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,25 @@
-let topics = "HTML, CSS, Git JavaScript";
-console.log(topics);
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+// console.log(topics);
+
+function listTopics() {
+  for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+  }
+}
+
+function selectTopic() {
+  if (topics === "HTML") {
+    console.log("Lets study HTML!");
+  } else if (topics === "CSS") {
+    console.log("Lets study CSS!");
+  } else if (topics === "Git") {
+    console.log("Lets study Git!");
+  } else if (topics === "JavaScript") {
+    console.log("Lets study JavaScript!");
+  } else {
+    console.log("Please Try Again!");
+  }
+}
+
+listTopics()
+// selectTopic()
